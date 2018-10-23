07503b59e800a9461a19d732398a7d2e77a70559
Bartłomiej Bigda <bartlomiej.bigda@promity.pl>
2018-10-23 18:31:50 +0200

    basic findAll integration test

--- src/test/java/todo/TodoItemRepositoryIntegrationTest.java
+++ src/test/java/todo/TodoItemRepositoryIntegrationTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -20,8 +22,20 @@ public class TodoItemRepositoryIntegrationTest {
     private TodoItemRepository employeeRepository;
 
     @Test
-    public void test() {
-        assertThat(true).isEqualTo(true);
+    public void whenFindAll_thenReturnAll() {
+        // given
+        TodoItem item1 = new TodoItem();
+        TodoItem item2 = new TodoItem();
+        entityManager.persist(item1);
+        entityManager.persist(item2);
+        entityManager.flush();
+
+        // when
+        List<TodoItem> found = (List<TodoItem>) employeeRepository.findAll();
+
+        // then
+        System.out.println(found);
+        assertThat(found.size()).isEqualTo(2);
     }
 
 
