5a32ad9f60b09ce4fec5fb237be8e804bb741b9e
Bartłomiej Bigda <bartlomiej.bigda@promity.pl>
2019-07-01 13:47:45 +0200

    server port configuration

--- src/main/java/todo/Application.java
+++ src/main/java/todo/Application.java
@@ -8,7 +8,9 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
         System.out.println("Backend ready on port 8080 !");
+        System.out.println(args);
     }
 }
 
